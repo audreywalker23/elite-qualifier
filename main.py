3203291bd8448e9e72a948b2cdadf32bf795c81a
Audrey Walker <90425187+audreywalker23@users.noreply.github.com>
2021-10-26 01:41:44 +0000

    I added a time manage system and created a name and assist function

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+start_time = time.time()
+print("--- %s seconds ---" % (time.time() - start_time))
+name=input('Helo friend, What is your name')
+print(f'Well hello {name} I am Archie how may I assist you today?')
+assist=input()
+print(f'Well ok I can look and see if I have {assist} for you')
+
+ 
